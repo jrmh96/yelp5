a04334b20e9a5c4d477dfefd6068a49d5cbdb09f
jeremyhu <jrmh96@gmail.com>
2017-03-28 20:07:03 -0400

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var mid = require('../middleware');
+
+//GET /
+router.get('/', function(req, res, next){
+    return res.render('mainpage', {pageTitle : 'mainpage'});
+});
