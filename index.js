0544dc62d2a127a328eb1104539acd3c5b50ec0e
jeremyhu <jrmh96@gmail.com>
2017-04-02 17:51:57 -0400

    remove xmlhttprequest

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var path = require('path');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var app = express();
 
 app.engine('.html', require('ejs').__express);
