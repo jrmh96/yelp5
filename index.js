8bfccb507ce64212a92566b9fe3e74ee46784858
jeremyhu <jrmh96@gmail.com>
2017-04-01 20:51:32 -0400

    try

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ app.get('/', function(req, res, next){
 //     next(err);
 // });
 
+router.get('/google8947d3762b9e857f.html', function(req, res, next){
+    return res.render('google8947d3762b9e857f.html');
+});
+
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     var message = err.message
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@ router.get('/', function(req, res, next){
     return res.render('mainpage.html', {pageTitle : 'mainpage'});
 });
 
-router.get('/google8947d3762b9e857f.html', function(req, res, next){
-    return res.render('google8947d3762b9e857f.html');
-});
+
 
 module.exports = router;
